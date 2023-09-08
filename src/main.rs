79eadd644e9e91bc50c31d79419c799737103944
9ziggy9 <davidarogers@protonmail.com>
2023-09-08 09:41:27 -0700

    resizable text windows

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
-use cursive::traits::Nameable;
-use cursive::views::{TextView, HideableView, NamedView};
-use cursive::view::Selector;
+use cursive::views::{TextView, HideableView, ResizedView};
+use cursive::view::{Selector, Resizable, Nameable};
 use cursive::{Cursive, CursiveExt};
-
-// Type aliases
-type NamedHideable = NamedView<HideableView<TextView>>;
 
 // Config file paths.
 const PATH_COLOR_SCHEME: &str = include_str!("colors.toml");
 
-fn m_init_hello_open() -> NamedHideable {
-    let m = TextView::new("h to open hello!");
-    return HideableView::new(m).with_name("hello");
+fn m_init_hello_open(a: &mut Cursive) {
+    let m = TextView::new("h to open hello!").full_screen();
+    a.add_layer(HideableView::new(m).with_name("hello"));
 }
 
 fn main() {
-    let hello = m_init_hello_open();
     let mut app = Cursive::new();
+    m_init_hello_open(&mut app);
 
     app.load_toml(PATH_COLOR_SCHEME).unwrap();
-    app.add_layer(hello);
 
     app.add_global_callback('`', Cursive::toggle_debug_console);
     app.add_global_callback('q', |s| s.quit());
     app.add_global_callback('t', |s| {
         s.call_on(
             &Selector::Name("hello"),
-            |view: &mut HideableView<TextView>| {
+            |view: &mut HideableView<ResizedView<TextView>>| {
                 view.set_visible(!view.is_visible());
             }
         );
     });
+
     app.run();
 }
